6bc8608ea902553f508816f72656c0c9f139d099
Udaykiran <peddintiudaykiran176@gmail.com>
2025-08-13 22:01:56 +0530

    added

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,62 +1,339 @@
-import { db } from "@/db/drizzle";
+import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+
 import * as schema from "@/db/schema";
 
-const seedCourses = async () => {
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql, { schema });
+
+const main = async () => {
   try {
-    console.log("Seeding courses...");
-    
-    const courseData = [
-      {
-        title: "Build your own circuit",
-        imageSrc: "/images/courses/circuit.jpg",
-        thumbnailSrc: "/images/thumbnails/circuit-thumb.jpg",
-        description: "Learn the fundamentals of electronic circuits and build your own projects from scratch. Perfect for beginners!",
-        price: "1999"
-      },
-      {
-        title: "Introduction to sensors",
-        imageSrc: "/images/courses/sensors.jpg",
-        thumbnailSrc: "/images/thumbnails/sensors-thumb.jpg",
-        description: "Discover how different sensors work and how to integrate them into your electronic projects.",
-        price: "1799"
-      },
-      {
-        title: "Wire & wireless bot",
-        imageSrc: "/images/courses/bot.jpg",
-        thumbnailSrc: "/images/thumbnails/bot-thumb.jpg",
-        description: "Build and program your own robot that can be controlled both with wires and wirelessly.",
-        price: "2499"
-      },
-      {
-        title: "Step into coding",
-        imageSrc: "/images/courses/coding.jpg",
-        thumbnailSrc: "/images/thumbnails/coding-thumb.jpg",
-        description: "Start your programming journey with this beginner-friendly introduction to coding concepts.",
-        price: "1499"
-      },
-      {
-        title: "Build a Robot",
-        imageSrc: "/images/courses/robot.jpg",
-        thumbnailSrc: "/images/thumbnails/robot-thumb.jpg",
-        description: "Comprehensive course on building and programming your first robot from scratch.",
-        price: "2999"
+    console.log("Seeding database");
+
+    // Delete all existing data
+    await Promise.all([
+      db.delete(schema.challenges),
+      db.delete(schema.units),
+      db.delete(schema.lessons),
+      db.delete(schema.courses),
+      db.delete(schema.challengeOptions),
+      db.delete(schema.userSubscription),
+    ]);
+
+    // Insert courses
+    const courses = await db
+      .insert(schema.courses)
+      .values([
+        { title: "Spanish", imageSrc: "/es.svg" },
+        { title: "French", imageSrc: "/fr.svg" },
+        { title: "Italian", imageSrc: "/it.svg" },
+      ])
+      .returning();
+
+    // For each course, insert units
+    for (const course of courses) {
+      const units = await db
+        .insert(schema.units)
+        .values([
+          {
+            courseId: course.id,
+            title: "Unit 1",
+            description: `Learn the basics of ${course.title}`,
+            order: 1,
+          },
+          {
+            courseId: course.id,
+            title: "Unit 2",
+            description: `Learn intermediate ${course.title}`,
+            order: 2,
+          },
+          
+        ])
+        .returning();
+
+      // For each unit, insert lessons
+      for (const unit of units) {
+        const lessons = await db
+          .insert(schema.lessons)
+          .values([
+            { unitId: unit.id, title: "Nouns", order: 1 },
+            { unitId: unit.id, title: "Verbs", order: 2 },
+            { unitId: unit.id, title: "Adjectives", order: 3 },
+            { unitId: unit.id, title: "Phrases", order: 4 },
+            { unitId: unit.id, title: "Sentences", order: 5 },
+          ])
+          .returning();
+
+        // For each lesson, insert challenges
+        for (const lesson of lessons) {
+          const challenges = await db
+            .insert(schema.challenges)
+            .values([
+              {
+                lessonId: lesson.id,
+                type: "SELECT",
+                question: 'Which one of these is "the man"?',
+                order: 1,
+              },
+              {
+                lessonId: lesson.id,
+                type: "SELECT",
+                question: 'Which one of these is "the woman"?',
+                order: 2,
+              },
+              {
+                lessonId: lesson.id,
+                type: "SELECT",
+                question: 'Which one of these is "the boy"?',
+                order: 3,
+              },
+              {
+                lessonId: lesson.id,
+                type: "ASSIST",
+                question: '"the man"',
+                order: 4,
+              },
+              {
+                lessonId: lesson.id,
+                type: "SELECT",
+                question: 'Which one of these is "the zombie"?',
+                order: 5,
+              },
+              {
+                lessonId: lesson.id,
+                type: "SELECT",
+                question: 'Which one of these is "the robot"?',
+                order: 6,
+              },
+              {
+                lessonId: lesson.id,
+                type: "SELECT",
+                question: 'Which one of these is "the girl"?',
+                order: 7,
+              },
+              {
+                lessonId: lesson.id,
+                type: "ASSIST",
+                question: '"the zombie"',
+                order: 8,
+              },
+            ])
+            .returning();
+
+          // For each challenge, insert challenge options
+          for (const challenge of challenges) {
+            if (challenge.order === 1) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "el hombre",
+                  imageSrc: "/man.svg",
+                  audioSrc: "/es_man.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "la mujer",
+                  imageSrc: "/woman.svg",
+                  audioSrc: "/es_woman.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el chico",
+                  imageSrc: "/boy.svg",
+                  audioSrc: "/es_boy.mp3",
+                },
+              ]);
+            }
+
+            if (challenge.order === 2) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "la mujer",
+                  imageSrc: "/woman.svg",
+                  audioSrc: "/es_woman.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el chico",
+                  imageSrc: "/boy.svg",
+                  audioSrc: "/es_boy.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el hombre",
+                  imageSrc: "/man.svg",
+                  audioSrc: "/es_man.mp3",
+                },
+              ]);
+            }
+
+            if (challenge.order === 3) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "la mujer",
+                  imageSrc: "/woman.svg",
+                  audioSrc: "/es_woman.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el hombre",
+                  imageSrc: "/man.svg",
+                  audioSrc: "/es_man.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "el chico",
+                  imageSrc: "/boy.svg",
+                  audioSrc: "/es_boy.mp3",
+                },
+              ]);
+            }
+
+            if (challenge.order === 4) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "la mujer",
+                  audioSrc: "/es_woman.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "el hombre",
+                  audioSrc: "/es_man.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el chico",
+                  audioSrc: "/es_boy.mp3",
+                },
+              ]);
+            }
+
+            if (challenge.order === 5) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el hombre",
+                  imageSrc: "/man.svg",
+                  audioSrc: "/es_man.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "la mujer",
+                  imageSrc: "/woman.svg",
+                  audioSrc: "/es_woman.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "el zombie",
+                  imageSrc: "/zombie.svg",
+                  audioSrc: "/es_zombie.mp3",
+                },
+              ]);
+            }
+
+            if (challenge.order === 6) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "el robot",
+                  imageSrc: "/robot.svg",
+                  audioSrc: "/es_robot.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el zombie",
+                  imageSrc: "/zombie.svg",
+                  audioSrc: "/es_zombie.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el chico",
+                  imageSrc: "/boy.svg",
+                  audioSrc: "/es_boy.mp3",
+                },
+              ]);
+            }
+
+            if (challenge.order === 7) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "la nina",
+                  imageSrc: "/girl.svg",
+                  audioSrc: "/es_girl.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el zombie",
+                  imageSrc: "/zombie.svg",
+                  audioSrc: "/es_zombie.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el hombre",
+                  imageSrc: "/man.svg",
+                  audioSrc: "/es_man.mp3",
+                },
+              ]);
+            }
+
+            if (challenge.order === 8) {
+              await db.insert(schema.challengeOptions).values([
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "la mujer",
+                  audioSrc: "/es_woman.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: true,
+                  text: "el zombie",
+                  audioSrc: "/es_zombie.mp3",
+                },
+                {
+                  challengeId: challenge.id,
+                  correct: false,
+                  text: "el chico",
+                  audioSrc: "/es_boy.mp3",
+                },
+              ]);
+            }
+          }
+        }
       }
-    ];
-
-    // Insert courses into the database
-    for (const course of courseData) {
-      await db.insert(schema.courses).values(course);
-      console.log(`Added course: ${course.title}`);
     }
-
-    console.log("✅ Seeding completed successfully!");
+    console.log("Database seeded successfully");
   } catch (error) {
-    console.error("❌ Error seeding courses:", error);
-    process.exit(1);
-  } finally {
-    await db.$cache
-    process.exit(0);
+    console.error(error);
+    throw new Error("Failed to seed database");
   }
 };
 
-seedCourses();
+void main();
