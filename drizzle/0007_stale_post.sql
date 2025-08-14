2d736e7fcd313e16955a2fba5e3a06fc703586b6
Udaykiran <peddintiudaykiran176@gmail.com>
2025-08-14 15:07:51 +0530

    ok

---
+++ drizzle/0007_stale_post.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "user_progress" (
+	"user_id" text PRIMARY KEY NOT NULL,
+	"user_name" text DEFAULT 'User' NOT NULL,
+	"user_image_src" text DEFAULT '/mascot.svg' NOT NULL,
+	"active_course_id" integer,
+	"points" integer DEFAULT 0 NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "user_progress" ADD CONSTRAINT "user_progress_active_course_id_courses_id_fk" FOREIGN KEY ("active_course_id") REFERENCES "public"."courses"("id") ON DELETE cascade ON UPDATE no action;
